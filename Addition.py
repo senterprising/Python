427f5cec2a285379eb1b9681fcde9606e5d60adb
tgsrikanth <tg.srikanth26@gmail.com>
2018-02-09 14:10:48 +0530

    added Addition.py

---
+++ Addition.py
@@ -0,0 +1,11 @@
+#Program to add two numbers
+
+#Assigning a value to the variable
+x = 20.6
+y = 25.8
+
+#Adding two numbers
+sum = x + y
+
+#Displaying the Output
+print('Sum of {0} and {1} is {2}'.format(x,y,sum))
